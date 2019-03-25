981207e3f6402e9aa3c6db9ade0114af2e35722c
Michael Cuneo <cuneo24@yahoo.com>
2019-03-24 23:47:54 -0400

    Practice video example commits

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/example', function () {
+    return view('abc');
+});
+
+Route::get('/books', 'BookController@index');
+
+Route::get('/books/{title}', 'BookController@show');
